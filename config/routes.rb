968bfd5cd1ff8b92f06c64952aa873f2a4f7757d
AmiasYaska <amiasyaska@gmail.com>
2024-07-09 19:29:33 +0300

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :locations
   devise_for :owners
   devise_for :customers
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -6,6 +7,10 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
+  authenticated :owner do
+    root to: "locations#index", as: :authenticated_owner_root
+  end
 
   # Defines the root path route ("/")
   root "pages#home"
