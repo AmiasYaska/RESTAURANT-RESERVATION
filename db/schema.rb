cb217cddc72d3e28cc4610738cc842389b7264f3
AmiasYaska <amiasyaska@gmail.com>
2024-07-14 21:35:17 +0300

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_14_175457) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_14_183450) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,4 +48,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_14_175457) do
     t.index ["reset_password_token"], name: "index_owners_on_reset_password_token", unique: true
   end
 
+  create_table "reservations", force: :cascade do |t|
+    t.bigint "location_id", null: false
+    t.bigint "customer_id", null: false
+    t.datetime "reservation_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_reservations_on_customer_id"
+    t.index ["location_id"], name: "index_reservations_on_location_id"
+  end
+
+  add_foreign_key "reservations", "customers"
+  add_foreign_key "reservations", "locations"
 end
