edadedfa8d22439638467ba7bb819a7de30f3d29
AmiasYaska <amiasyaska@gmail.com>
2024-07-15 22:20:46 +0300

    changes

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,7 +3,7 @@ class Location < ApplicationRecord
 
     def within_operating_hours(reservation_date)
         reservation_time = Time.parse(reservation_date.to_s)
-        reservation_time.strftime("%H%M%S%N") > opening_time.strftime("%H%M%S%N") && 
-        reservation_time.strftime("%H%M%S%N") < closing_time.strftime("%H%M%S%N") 
+        reservation_time.strftime("%H%M%S") > opening_time.strftime("%H%M%S") && 
+        reservation_time.strftime("%H%M%S") < closing_time.strftime("%H%M%S") 
     end
 end
