f262d58097b26ad59e88a6c1c160f3cf9d187765
AmiasYaska <amiasyaska@gmail.com>
2024-07-15 18:29:13 +0300

    changes

--- app/controllers/locations/reservations_controller.rb
+++ app/controllers/locations/reservations_controller.rb
@@ -9,7 +9,12 @@ module Locations
         def create
             @reservation = @location.reservations.new(reservation_date: params[:reservation_date])
 
-            
+            if @location.operating_hours(@reservation.reservation_date)
+                render :new, alert: "Successful"
+
+            else
+                render :new, alert: "The time you chose is outside the operting hours"
+            end
 
         end
 
--- app/models/location.rb
+++ app/models/location.rb
@@ -2,6 +2,6 @@ class Location < ApplicationRecord
     has_many :reservations
 
     def operating_hours(reservation_date)
-        
+        reservation_date.to_time < opening_time && reservation_date.to_time < closing_time
     end
 end
